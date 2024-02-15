4268287f53a56c9590b4c072f7d7da5bfbd650b8
jaime gonzalez fabregas <jaimegonzalezfabregas@gmail.com>
2024-02-15 20:21:57 +0100

    first commit

---
+++ plan.js
@@ -0,0 +1,82 @@
+const plan = {
+    "Monday": {
+        "subtitle": "Chest and Triceps",
+        "exercises": [
+            {
+                "name": "Bench Press",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Incline Bench Press",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Chest Fly",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Tricep Dips",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Tricep Pushdown",
+                "sets": 4,
+                "reps": 8
+            }
+        ]
+    },
+    "Wednesday": {
+        "subtitle": "Back and Biceps",
+        "exercises": [
+            {
+                "name": "Deadlift",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Pull-ups",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Barbell Curl",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Hammer Curl",
+                "sets": 4,
+                "reps": 8
+            }
+        ]
+    },
+    "Thursday": {
+        "subtitle": "Legs",
+        "exercises": [
+            {
+                "name": "Squat",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Leg Press",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Leg Curl",
+                "sets": 4,
+                "reps": 8
+            },
+            {
+                "name": "Leg Extension",
+                "sets": 4,
+                "reps": 8
+            }
+        ]
+    },
+}
